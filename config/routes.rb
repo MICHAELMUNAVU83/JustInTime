199d5c661112ff47dac1cfdee21588e8bca8b738
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-03-27 07:58:03 +0300

    Started styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,33 @@
 Rails.application.routes.draw do
-  resources :donations
-  devise_for :users
-  root "donations#index"
+ 
+ 
+  # resources :donations
+  devise_for  :users do
+    authenticated :user do
+      root to: "donations#index", as: :authenticated_root
+    end
+
+    unauthenticated do
+      root to: "pages#home", as: :unauthenticated_root
+    end
+
+
+  end
+
+  
+  # authenticated :user do
+  #   root to: "donations#index", as: :authenticated_root
+  # end
+
+  # unauthenticated do
+  #   root to: "pages#home", as: :unauthenticated_root
+  # end
+
+
+
+  
+
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_27_041206) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_27_042522) do
   create_table "donations", force: :cascade do |t|
     t.string "type_of_donation"
     t.integer "weight"
@@ -36,9 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_27_041206) do
     t.string "county"
     t.string "sub_county"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"],
-            name: "index_users_on_reset_password_token",
-            unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "donations", "users"
